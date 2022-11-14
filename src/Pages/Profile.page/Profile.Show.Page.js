23a4a6dea42316b3a3f91392e24e81bd0e9e875e
Gamik Budhathoki <gamiksingz@gmail.com>
2022-11-14 13:16:10 -0500

    added image in profile

--- src/Pages/Profile.page/Profile.Show.Page.js
+++ src/Pages/Profile.page/Profile.Show.Page.js
@@ -1,18 +1,30 @@
-import React,{useContext, useEffect} from "react";
+import React,{useContext, useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import { ProfileContext } from "../../context/ProfileContext";
+import ProfilePictureUpload from "./ProfilePictureUpload.Page";
 
 export default function ProfileShow ({id}){
-
+    const [uploadFile,setUploadFile] = useState(null);
     const {profile,getProfile} = useContext(ProfileContext);
     const {user} = useContext(AuthContext);
     const navigate = useNavigate();
+    
+    const uploadedFile = (file)=>{
+        setUploadFile(file);
+    }
+
     useEffect(()=>{getProfile(user.token);},[]);
+    console.log(uploadFile);
     return (
         <div>
             <h1>profile</h1>
+            <div>
+            {uploadFile && <img alt="uploaded" src={URL.createObjectURL(uploadFile)} className="uploadImg"/>}
+            <ProfilePictureUpload uploadedFile={uploadedFile} />
+            </div>
             <p>{JSON.stringify(profile)}</p>
+            <img className="uploadImg" src={`http://localhost:8800/images/${profile.displayImage}`} alt="" />
             <button onClick={()=>{navigate("/profile/update")}}>Update Profile</button>
         </div>
     );
